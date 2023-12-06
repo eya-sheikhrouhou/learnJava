c15305aa5357032b5486ffd2ec2a5dfb0ad7943c
eya <eya.sheikhrouhou@esprit.tn>
2023-12-06 22:34:46 +0100

    prosit 9 avec correction

---
+++ src/IGestion.java
@@ -0,0 +1,17 @@
+public interface IGestion<T> {
+
+    void ajouterEmploye(T t);
+
+    boolean rechercherEmploye(String nom);
+
+    boolean rechercherEmploye(T t);
+
+    void supprimerEmploye(T t);
+
+    void displayEmploye();
+
+
+    void trierEmployeParId();
+
+    void trierEmployeParNomDepartementEtGrade();
+}
